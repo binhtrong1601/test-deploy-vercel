28b63f0c661722977c35ade3e5a9fa702e37f6f4
binh-trong <binhtrong1601@gmail.com>
2023-04-25 23:16:45 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    STUDENT_API: "http://localhost:8080/api/v1/students",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
